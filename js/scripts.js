12699957482336007ca9f9e8628e01fd8f0ff990
Ian Wilcox and Vichitra Pool <ian+vichitra@>
2016-08-08 13:51:26 -0700

    added BMI Temp and gallons

--- js/scripts.js
+++ js/scripts.js
@@ -41,3 +41,24 @@ var multiply = function(number1, number2) {
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 alert(multiply(number1, number2));
+
+var BMI= function(number1, number2) {
+  return number1/(number2 * number2);
+};
+
+var number1 =parseInt(prompt('Enter your weight in kilograms:'));
+var number2 =parseInt(prompt('Enter your height in meter:'));
+
+alert(BMI(number1, number2));
+
+
+
+var Fahrenheit = function(number1) { return (number1 * 1.8) + 32;};
+var number1 = parseInt(prompt("Enter Celsius"));
+alert(Fahrenheit(number1));
+
+
+
+var liters = function(number1) { return (number1 * 3.78541);};
+var number1 = parseInt(prompt("enter gallons"));
+alert(liters(number1));
